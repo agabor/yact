662784434f8397aed226b79aeaff251a98476c1a
Gabor Angyal <gabor.angyal@codesharp.dev>
2026-03-03 09:27:50 +0100

    make block parsing more flexible

--- logic/codeblock.go
+++ logic/codeblock.go
@@ -11,7 +11,8 @@ import (
 
 var unknownFileCounter = 0
 
-const BlockDelimiter = "``" + "``"
+const BlockDelimiterMin = "``" + "`"
+const BlockDelimiter = BlockDelimiterMin + "`"
 
 type CodeBlock struct {
 	Path    string
@@ -117,4 +118,31 @@ func (cb *CodeBlock) Write(safe bool) error {
 
 	fmt.Printf("Written: %s\n", filePath)
 	return nil
+}
+func ParseCodeBlocks(response string) []CodeBlock {
+	lines := strings.Split(response, "\n")
+	var codeBlocks = make([]CodeBlock, 0)
+	var lineBuffer = make([]string, 0)
+	inBlock := false
+	for _, line := range lines {
+		if strings.HasPrefix(strings.TrimSpace(line), BlockDelimiterMin) {
+			if inBlock {
+				if len(lineBuffer) > 0 {
+					codeBlocks = append(codeBlocks, linesToCodeBlock(lineBuffer))
+				}
+				inBlock = false
+				lineBuffer = make([]string, 0)
+			} else {
+				inBlock = true
+			}
+		} else if inBlock {
+			lineBuffer = append(lineBuffer, line)
+		}
+	}
+
+	if inBlock && len(lineBuffer) > 0 {
+		codeBlocks = append(codeBlocks, linesToCodeBlock(lineBuffer))
+	}
+
+	return codeBlocks
 }
