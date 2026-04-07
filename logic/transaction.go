7c69237b69b2b2915b89087dbfd712eed8df3e00
Gabor Angyal <gabor.angyal@codesharp.dev>
2026-04-07 14:29:13 +0200

    filter files after plan

--- logic/transaction.go
+++ logic/transaction.go
@@ -31,6 +31,14 @@ func LoadContextForAct() ([]Transaction, error) {
 		lastPlan := strings.TrimSpace(getLastPlan(transactions))
 		if lastPlan != "" {
 			tx.Request = []string{lastPlan}
+
+			filteredContext := []CodeFile{}
+			for _, file := range tx.Context {
+				if strings.Contains(lastPlan, file.Path) {
+					filteredContext = append(filteredContext, file)
+				}
+			}
+			tx.Context = filteredContext
 		}
 		return []Transaction{tx}, nil
 	}
