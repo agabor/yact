199a93c92c7dff6b3dcf7883152bf4efbe105472
Gabor Angyal <gabor.angyal@codesharp.dev>
2026-04-22 11:05:07 +0200

    serialize to text file

--- logic/contextstore.go
+++ logic/contextstore.go
@@ -1,12 +1,12 @@
 package logic
 
 import (
-	"encoding/json"
 	"os"
-	"path/filepath"
+	"strings"
 )
 
 const contextPath = ".yact"
+const separator = "=========="
 
 func LoadContext() (Transaction, error) {
 	data, err := os.ReadFile(contextPath)
@@ -17,24 +17,54 @@ func LoadContext() (Transaction, error) {
 		return Transaction{}, err
 	}
 
-	var transaction Transaction
-	if err := json.Unmarshal(data, &transaction); err != nil {
-		return Transaction{}, err
+	content := string(data)
+	lines := strings.Split(strings.TrimSpace(content), "\n")
+
+	transaction := Transaction{
+		Request: []string{},
+		Context: []string{},
+	}
+
+	separatorIndex := -1
+	for i, line := range lines {
+		if line == separator {
+			separatorIndex = i
+			break
+		}
+	}
+
+	if separatorIndex == -1 {
+		return transaction, nil
+	}
+
+	for i := 0; i < separatorIndex; i++ {
+		if strings.TrimSpace(lines[i]) != "" {
+			transaction.Context = append(transaction.Context, lines[i])
+		}
+	}
+
+	if separatorIndex+1 < len(lines) && strings.TrimSpace(lines[separatorIndex+1]) != "" {
+		transaction.Request = append(transaction.Request, lines[separatorIndex+1])
 	}
 
 	return transaction, nil
 }
 
 func SaveContext(transaction Transaction) error {
-	dir := filepath.Dir(contextPath)
-	if err := os.MkdirAll(dir, 0755); err != nil {
-		return err
+	var builder strings.Builder
+
+	for _, contextPath := range transaction.Context {
+		builder.WriteString(contextPath)
+		builder.WriteString("\n")
 	}
 
-	data, err := json.MarshalIndent(transaction, "", "  ")
-	if err != nil {
-		return err
+	builder.WriteString(separator)
+	builder.WriteString("\n")
+
+	if len(transaction.Request) > 0 && strings.TrimSpace(transaction.Request[0]) != "" {
+		builder.WriteString(transaction.Request[0])
+		builder.WriteString("\n")
 	}
 
-	return os.WriteFile(contextPath, data, 0644)
+	return os.WriteFile(contextPath, []byte(builder.String()), 0644)
 }
